a430b0d7938c1b47d2bd973987cd03742befc65e
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-03-23 15:31:45 -0400

    create model.py file

---
+++ server/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from config import db, bcrypt
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
