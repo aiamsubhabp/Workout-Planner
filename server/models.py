4beb55f0c60734da6d768f74b05748592afce47a
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-03-23 16:49:10 -0400

    create User, Exercise, UserExercises classes in model.py

--- server/models.py
+++ server/models.py
@@ -5,4 +5,33 @@ from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, unique = True, nullable = False)
+    _password_hash = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User {self.id}, {self.username}>'
+
+class Exercise(db.Model, SerializerMixin):
+    __tablename__ = 'exercises'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    muscle_group = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User {self.id}, {self.name}, {self.muscle_group}>'
+
+class UserExercises(db.Model, SerializerMixin):
+    __tablename__ = 'user_exercises'
+
+    id = db.Column(db.Integer, primary_key = True)
+    sets = db.Column(db.Integer)
+    reps = db.Column(db.Integer)
+    notes = db.Column(db.String, nullable = True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'))
+
     
